e5462371ecf099c557641b8bb1bcf8f8b162db58
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-08-29 12:58:45 -0600

    Finished adding routes and began adding controllers.

--- models/User.js
+++ models/User.js
@@ -1,2 +1,52 @@
 const { Schema, model } = require('mongoose');
-// const dateFormat = require('../utils/dateFormat');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            }
+        }
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// UserSchema.virtual('thoughtCount').get(function() {
+//     return this.thoughts.reduce(
+//         (total, thought) => total + thought.reactions.length + 1,
+//         0
+//     );
+// });
+
+
+
+const User = model('User', UserSchema);
+
+module.exports = User;
